6c4db8beb4dc3bd20d8785630f015dc322ddea0c
byacspe <byacspe@163.com>
2026-05-24 01:06:26 +0800

    Add Jenkins pipeline

--- docs-core/src/main/java/com/sismics/docs/core/dao/AuthenticationTokenDao.java
+++ docs-core/src/main/java/com/sismics/docs/core/dao/AuthenticationTokenDao.java
@@ -45,17 +45,16 @@ public class AuthenticationTokenDao {
 
     /**
      * Deletes the authentication token.
-     * 
+     *
      * @param authenticationTokenId Authentication token ID
-     * @throws Exception
      */
-    public void delete(String authenticationTokenId) throws Exception {
+    public void delete(String authenticationTokenId) {
         EntityManager em = ThreadLocalContext.get().getEntityManager();
         AuthenticationToken authenticationToken = em.find(AuthenticationToken.class, authenticationTokenId);
         if (authenticationToken != null) {
             em.remove(authenticationToken);
         } else {
-            throw new Exception("Token not found: " + authenticationTokenId);
+            throw new RuntimeException("Token not found: " + authenticationTokenId);
         }
     }
 
@@ -63,7 +62,6 @@ public class AuthenticationTokenDao {
      * Deletes old short lived tokens.
      *
      * @param userId User ID
-     * @throws Exception
      */
     public void deleteOldSessionToken(String userId) {
         StringBuilder sb = new StringBuilder("delete from T_AUTHENTICATION_TOKEN AS ato ");
@@ -79,10 +77,9 @@ public class AuthenticationTokenDao {
     }
 
     /**
-     * Deletes old short lived tokens.
+     * Updates the last connection date of a token.
      *
      * @param id Token id
-     * @throws Exception
      */
     public void updateLastConnectionDate(String id) {
         StringBuilder sb = new StringBuilder("update T_AUTHENTICATION_TOKEN ato ");
